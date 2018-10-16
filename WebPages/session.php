69fc6a0f8f88a3efe7022afbb71cf98274cfbb0f
brandonlitwin <brandon.litwin1@marist.edu>
2018-10-16 15:09:20 -0400

    Removed unnecessary stuff from session file

--- WebPages/session.php
+++ WebPages/session.php
@@ -1,10 +1,6 @@
 <?php
 
     //Establish a connection to the IRB database
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
-    $dbName = "postgres";
     $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres");
 
     //Check if the session is active
