b54f54a3a698acdaa321d67f9b62ddc39ed24828
nickklacik <nickklacik@gamil.com>
2018-10-17 11:15:05 -0400

    fix upload header

--- WebPages/upload.php
+++ WebPages/upload.php
@@ -13,20 +13,10 @@ if(empty($_SESSION['login_user'])) {
     <link rel="stylesheet" type="text/css" href="pablo.css">
   </head>
   <body>
-    <div style="height: 150px; width: 100%;">
-      <div style="text-align: center; height: 100px; background-color: white;">
-        <img src="pastyle.png" width="182px" height="100px">
-      </div>
-      <div style="text-align: center; line-height: 50px; height: 50px; background-color: orange;
-        border: 2px solid black;">
-        <span class="headerLink" onclick="window.location.href = 'index.php'">Home</span>
-        <span class="headerLink" onclick="alert('about')">About</span>
-        <span class="headerLink" onclick="window.location.href = 'view.php'">View</span>
-        <span class="headerLink" onclick="alert('help')">Help</span>
-        <span class="headerLink" onclick="window.location.href = 'logout.php'">Logout</span>
-        
-      </div>
-    </div>
+    <?php
+      require("functions.php");
+      createHeader();
+    ?>
     <br><br>
     <form action="uploadImages.php" method="post" enctype="multipart/form-data">
         Select image to upload:
